7187fde7f3f646c9a4da86c5f83044ec27229d4f
jackson <9527380+Jaksuhn@users.noreply.github.com>
2022-03-17 14:45:19 -0400

    fixed accidental removal of +1/+2 suffix

--- link-fix.py
+++ link-fix.py
@@ -6,9 +6,10 @@ def convert_spaces(match_obj):
     category = match_obj.group(1)
     num = match_obj.group(2)
     item = match_obj.group(3)
+    item_fix = item
     if item.endswith(" +1") or item.endswith(" +2"):
-        item = item[: -len(" +1")]
-    return f'<li data-id="{category}_{num}"><a href="{fextra + item.replace(" ", "+")}">{item}</a></li>'
+        item_fix = item[: -len(" +1")]
+    return f'<li data-id="{category}_{num}"><a href="{fextra + item_fix.replace(" ", "+")}">{item}</a></li>'
 
 
 with open("index.html") as r:
